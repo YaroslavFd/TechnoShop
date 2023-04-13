4917e2d14e14f62ca8798632e98dbf3df8757e22
Yaroslav <yaroslav.web9@gmail.com>
2023-04-13 18:42:12 +0300

    added countdown timer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import CountdownTimer from '../components/CountdownTimer';
 import ProductCard from '../components/ProductCard';
 
 const Home = () => {
-  return <ProductCard />;
+  const targetDate = new Date('April 20, 2023 23:59:00');
+  return (
+    <>
+      <ProductCard />
+      <CountdownTimer targetDate={targetDate} />
+    </>
+  );
 };
 
 export default Home;
