e8e4495fbadab96f4d327ea537983257f3be82cf
kosykh-a <aleksandr.kosykh@nedra.digital>
2023-04-25 17:54:57 +0300

    feat: add example

--- src/UI/Icon/index.jsx
+++ src/UI/Icon/index.jsx
@@ -4,11 +4,13 @@ import DefaultIconComponent from './assets/eye.svg';
 
 import { IconStoreContext } from './utils/context';
 
-export const Icon = ({ name }) => {
+export const Icon = ({ name, className, style }) => {
   const iconStore = useContext(IconStoreContext);
 
   const IconComponent = useMemo(() => iconStore.get(name), [iconStore, name]);
   return (
-    <Suspense fallback={<DefaultIconComponent />}>{<IconComponent />}</Suspense>
+    <Suspense fallback={<DefaultIconComponent style={style} />}>
+      <IconComponent className={className} style={style} />
+    </Suspense>
   );
 };
